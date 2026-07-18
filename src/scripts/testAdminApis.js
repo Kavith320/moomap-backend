77481e5eab491b2f44fe221e822ea0d2979cafb7
Kavith320 <kavith.b.320udapola@gmail.com>
2026-07-18 10:33:15 +0530

    feat: support BACKEND_PORT in testAdminApis.js

--- src/scripts/testAdminApis.js
+++ src/scripts/testAdminApis.js
@@ -1,6 +1,6 @@
 // src/scripts/testAdminApis.js
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.BACKEND_PORT || process.env.PORT || 3000;
 const BASE_URL = `http://localhost:${port}`;
 
 async function runTests() {
